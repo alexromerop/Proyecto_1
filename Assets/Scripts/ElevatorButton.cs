ff6d9bb2c89d522d42e5f0acc7834f2dc416db6e
alex <alex.romero@enti.cat>
2021-03-08 19:59:58 +0100

    try_elevator

---
+++ Assets/Scripts/ElevatorButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ElevatorButton : MonoBehaviour
+{
+	public Elevator Elevator;
+
+	void OnTriggerEnter(Collider collider)
+	{
+		var go = collider.gameObject;
+		var prota_0 = go.GetComponent<porta_0>();
+
+		if (prota_0 != null && Elevator != null)
+		{
+			Elevator.CallElevator();
+		}
+	}
+}
--- Assets/Scripts/Player_movment.cs
+++ Assets/Scripts/Player_movment.cs
@@ -4,31 +4,38 @@ using UnityEngine;
 
 public class Player_movment : MonoBehaviour
 {
+    public enum Posession { Default, Rat, Skeleton, Zombie }
     public float Speed;
     public float JumpForce;
 
     private Rigidbody2D Rigidbody2D;
     private float horizontal;
     private float vertical;
+    public int Posession_ = 0;
+        
     public bool in_ground;
 
+    
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Posession_ = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+    horizontal = Input.GetAxisRaw("Horizontal");
         // if fly == true
         vertical = Input.GetAxisRaw("Vertical");
 
         if (horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-        else if (horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        //if (Possesion_ == 0) {
+            if (horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        //}
 
 
-        if (Physics2D.Raycast(transform.position, Vector3.down, 0.22f))
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.25f))
         {
             in_ground = true;
         }
