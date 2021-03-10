7e8610b2d62a00767294693dbf44f6a27a32b053
xavierenti <xavier.massaneda@enti.cat>
2021-03-10 14:02:35 +0100

    CheckPoints
    scrips de checkpoint

---
+++ Assets/Scripts/ChecPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChecPoint : MonoBehaviour
+{
+   private void OnTriggerEnter2D(Collider2D vollision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerRespawn>().ReachedCheckPoint(transform.position.x,transfor.position.y);
+        }
+    }
+}
