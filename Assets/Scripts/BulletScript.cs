b26d495895c5e4bfc6619bbc64dd3cb9821ce05b
alex <alex.romero@enti.cat>
2021-03-16 20:09:32 +0100

    Boss muere, se mueve con limites (sirven paara todos tag enemey)

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -9,6 +9,8 @@ public class BulletScript : MonoBehaviour
     private Rigidbody2D rigidbody2d;
     private Vector2 Direction;
 
+   
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Enemys/BossScript.cs
+++ Assets/Scripts/Enemys/BossScript.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
+   
+    public float Speed;
+    Rigidbody2D Rigidbody2D;
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Rigidbody2D.velocity = new Vector2(Speed, Rigidbody2D.velocity.y);
+
+
+
     }
-}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Plataform")
+        {
+            Speed *= -1;
+            this.transform.localScale = new Vector2(this.transform.localScale.x * -1, this.transform.localScale.y);
+        }
+    }
+   }
+  
+
