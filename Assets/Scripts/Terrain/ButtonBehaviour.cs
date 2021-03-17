a31184f36adf9bd4532b2b0a284e31c2cd123d79
Adria Sebastia <adriguiu97@gmail.com>
2021-03-17 02:43:09 +0100

    Button Obre porta
    Obre la maldita porta per fi

--- Assets/Scripts/Terrain/ButtonBehaviour.cs
+++ Assets/Scripts/Terrain/ButtonBehaviour.cs
@@ -5,26 +5,19 @@ using UnityEngine;
 public class ButtonBehaviour : MonoBehaviour
 {
     public GameObject door;
-    //private Vector3 direction;
-    //public float speed;
     public GameObject target;
+
     private Vector3 targetPos;
     // Start is called before the first frame update
     void Start()
     {
-        //direction = Vector3.up;
+        door = GameObject.Find("Door");
         targetPos = target.transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
     public void OpenDoor()
     {
         Debug.Log("Open Door");
-        door.transform.position += Vector3.MoveTowards(door.transform.position, targetPos, 2);
+        door.transform.position = Vector3.MoveTowards(door.transform.position, targetPos, 5);
 
     }
 }
