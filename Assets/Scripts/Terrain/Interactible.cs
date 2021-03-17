3c36609b6ec87237faf2d0854f94c17866c5a994
Adria Sebastia <adriguiu97@gmail.com>
2021-03-17 03:03:37 +0100

    Escena Video Prototipo Seguimiento
    Escena del Video Seguimiento

--- Assets/Scripts/Terrain/Interactible.cs
+++ Assets/Scripts/Terrain/Interactible.cs
@@ -27,14 +27,14 @@ public class Interactible : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Controlable"))
         {
             inRange = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Controlable"))
         {
             inRange = false;
         }
