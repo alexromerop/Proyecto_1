a4373f5b93d03a8de04975331188c772d0f7e5b5
Adria Sebastia <adriguiu97@gmail.com>
2021-03-27 16:58:23 +0100

    Scene MainMenu
    Falta Conectar los sliders a los futuros audios

---
+++ Assets/Scripts/UI Menu/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject menu;
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Map_test", LoadSceneMode.Single);
+        Debug.Log("PLAY!");
+    }
+
+    public void GoToCilckedButton(GameObject label)
+    {
+        menu.SetActive(false);
+        label.SetActive(true);
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void Back(GameObject label)
+    {
+        label.SetActive(false);
+        menu.SetActive(true);
+    }
+}
