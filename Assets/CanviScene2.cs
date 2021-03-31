05ca94a6716b827b6896a5de141ba8bfde0178b1
xavierenti <xavier.massaneda@enti.cat>
2021-03-31 19:09:07 +0200

    Menu de Nivells
    nomes faltaria tenir els nivells
    el primer et porta a mapa de prova
    si voleu ficar un piu per tirar enrrera aviseu

---
+++ Assets/CanviScene2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanviScene2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EsceneJuego()
+    {
+        SceneManager.LoadScene("");
+    }
+
+}
