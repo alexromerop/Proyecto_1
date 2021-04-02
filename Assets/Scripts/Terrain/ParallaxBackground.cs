8925a7ded8c9a6534a94273059fc58d93e864562
Adria Sebastia <adriguiu97@gmail.com>
2021-04-02 14:03:07 +0200

    Level 1 Desert Background +  Parallax Script
    Background + Parallax Script for more backgrounds in other levels

---
+++ Assets/Scripts/Terrain/ParallaxBackground.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    public Vector2 parallaxEffectMultiplayer;
+    public bool infiniteHorizontal;
+    public bool infiniteVertical;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
+    private float textureUnitSizeY;
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = (texture.width / sprite.pixelsPerUnit);
+        textureUnitSizeY = (texture.height / sprite.pixelsPerUnit);
+    }
+
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplayer.x, deltaMovement.y * parallaxEffectMultiplayer.y);
+        lastCameraPosition = cameraTransform.position;
+
+        if (infiniteHorizontal)
+        {
+            if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+            {
+                float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+                transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+            }
+        }
+
+        if (infiniteVertical)
+        {
+            if (Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY)
+            {
+                float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeX;
+                transform.position = new Vector3(cameraTransform.position.x, transform.position.y + offsetPositionY);
+            }
+        }
+    }
+}
