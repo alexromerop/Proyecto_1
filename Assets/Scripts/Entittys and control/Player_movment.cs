e150632408e48726c0b176681911a70e388feca3
alex <alex.romero@enti.cat>
2021-05-03 20:21:45 +0200

    Raton click en enemigo sale que no puedes

--- Assets/Scripts/Entittys and control/Player_movment.cs
+++ Assets/Scripts/Entittys and control/Player_movment.cs
@@ -26,9 +26,7 @@ public class Player_movment : MonoBehaviour
     public int Health = 1;
     private int sceneIndex;
 
-    public GameObject Info_to_show;
    
-    private float LastShoot;
 
 
     void Start()
@@ -159,29 +157,7 @@ public class Player_movment : MonoBehaviour
         //Destroy(gameObject);
     }
     
-    private void OnMouseOver()
-    {
-        //if (gameObject.tag.Equals("Player"))
-        {
-            if (Time.time > LastShoot + 2f)
-            {
-                GameObject boton = Instantiate(Info_to_show);
-                boton.gameObject.transform.parent = this.gameObject.transform;
-                boton.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x,
-                    this.gameObject.transform.position.y+0.35f, this.gameObject.transform.position.z);
-
-
-                LastShoot = Time.time;
-            }
-        }
-        
-       
-
-
-
-
-
-    }
+   
 
 
 }
