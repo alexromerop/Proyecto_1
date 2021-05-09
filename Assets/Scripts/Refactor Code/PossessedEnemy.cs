7c2713f53fcf12e46510207ac5182fe2e1fbdb54
Adria Sebastia <adriguiu97@gmail.com>
2021-05-09 14:29:58 +0200

    Prota se mueve hacia la paossesion

---
+++ Assets/Scripts/Refactor Code/PossessedEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PossessedEnemy : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.G))
+        {
+            Unposses();
+        }
+    }
+     
+    void Unposses()
+    {
+        GameObject instEnemy = Instantiate(enemy, transform.position, Quaternion.identity);
+        //GameObject player = GameObject.FindGameObjectWithTag("Player");
+        //player.transform.position = SetPlayerPosition(transform.position);
+        //player.SetActive(true);
+        GameObject instPlayer = Instantiate(player, SetPlayerPosition(transform.position), Quaternion.identity);
+        
+        Destroy(gameObject);
+    }
+    Vector3 SetPlayerPosition(Vector3 startPosition)
+    {
+        return startPosition + new Vector3(0,0.3f,0);
+    }
+}
--- Assets/Scripts/Refactor Code/PossessionBehaviour.cs
+++ Assets/Scripts/Refactor Code/PossessionBehaviour.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PossessionBehaviour : MonoBehaviour
 {
+    GameObject player;
+    public GameObject possedGameobject;
+    Transform wayPoint;
+    [SerializeField] float speed;
 
+
+    bool toPosses;
+    bool hover;
     private void Awake()
     {
         
@@ -12,12 +19,67 @@ public class PossessionBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (possedGameobject == null)
+        {
+            possedGameobject = GameObject.FindGameObjectWithTag("Controlable");
+        }
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            wayPoint = player.transform;
+        }
+
+        toPosses = false;
     }
 
+    private void FixedUpdate()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
+
+        if ((wayPoint.position - transform.position).sqrMagnitude < 0.1f) toPosses = true;
         
+
+        if (Input.GetMouseButtonDown(0) && hover == true)
+        {
+            if ((wayPoint.position - transform.position).sqrMagnitude < 2f){
+                StopAllCoroutines();
+                StartCoroutine(MoveTo(transform.position, speed));
+                hover = false;
+            }    
+        }
+
+        if (toPosses)
+        {
+            Posses();
+        }
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        Debug.Log("Mouse Over Something");
+        hover = true;
+    }
+
+    IEnumerator MoveTo(Vector2 destination, float speed)
+    {
+        while ((wayPoint.position - transform.position).sqrMagnitude > 0.05f)
+        {
+            wayPoint.position = Vector2.MoveTowards(wayPoint.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    void Posses()
+    {
+        GameObject instPossesed = Instantiate(possedGameobject, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        player.SetActive(false);
     }
 }
