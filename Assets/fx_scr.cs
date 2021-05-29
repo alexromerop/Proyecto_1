dd65f5707916b0809a8ad72315983767f15d7642
alex <alex.romero@enti.cat>
2021-05-29 12:29:05 +0200

    Water and fire fx

---
+++ Assets/fx_scr.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fx_scr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+
+
+    private Vector2 Direction;
+    private Rigidbody2D rigidbody2d;
+    public GameObject Player;
+    public bool Atacando;
+
+    // Start is called before the first frame update
+
+    private void Update()
+    {
+        
+    }
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        //this.transform.loca = Direction;
+    }
+    public void SetDirection(Vector3 direction)
+    {
+        Direction = direction;
+    }
+
+}
