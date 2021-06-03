3b9e50e378a9538dab58bb1dce4a9fe9fe8b58d6
Adria Sebastia <adriguiu97@gmail.com>
2021-06-03 15:30:50 +0200

    Humano dipara a rata + animacion disparo

--- Assets/Scripts/Enemys/Humano.cs
+++ Assets/Scripts/Enemys/Humano.cs
@@ -9,37 +9,52 @@ public class Humano : MonoBehaviour
     public GameObject Bullet;
     public int Health = 2;
     public Animator animator;
-    private float speed;
+    public float speed;
     public bool shoting;
 
     public AudioManager audioManager;
 
     private float LastShoot;
+    private Rigidbody2D rigidbody2D;
+    private Vector3 direction;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        //if (!shoting)
+        //{
+        //    rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+        //}
+    }
     private void Update()
     {
 
         for (int i = 0; i < Alie.Length; i++)
         {
-            Vector3 direction = Alie[i].transform.position - transform.position;
-            if (direction.x >= 0.0f)
+            direction = Alie[i].transform.position - transform.position;
+            if (direction.x >= 0.0f && direction.y < 0.3f)
             {
                 transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
                // Human.GetComponent<SpriteRenderer>().flipX = false;
             }
-            else { 
+            else if (direction.x < 0.0f && direction.y > 0.3f)
+            { 
                 transform.localScale = new Vector3(-0.8f, 0.8f, 0.8f);
                
             }
 
             float distance = Mathf.Abs(Alie[i].transform.position.x - transform.position.x);
-            if (distance < 3.0f && Time.time > LastShoot + 1.5f)
+            if (distance < 3.0f && Time.time > LastShoot + 1.5f && direction.y > -0.2f)
             {
-                shoting = false;
-                Shoot();
-                LastShoot = Time.time;
                 shoting = true;
+                animator.SetTrigger("Shoot");
+                StartCoroutine(ShootAnimation());
+                shoting = false;
+                LastShoot = Time.time;
             }
-            animator.SetBool("Shoting", shoting);
             animator.SetFloat("Speed", Mathf.Abs(speed));
             
 
@@ -51,19 +66,28 @@ public class Humano : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    private void Shoot()
+    private void Shoot(Vector3 directionTarget)
     {
         Debug.Log("Shoot");
-
-        Vector3 direction;
-        if (transform.localScale.x == 0.8f) direction = Vector3.right;
-        else direction = Vector3.left;
-
+        Vector3 bulletPosition = transform.position + directionTarget * 0.25f;
         //audioManager.PlayClicked();
-        GameObject instBullet = Instantiate(Bullet, transform.position + direction * 0.3f, Quaternion.identity);
-        instBullet.GetComponent<BulletScript>().SetDirection(direction);
+        GameObject instBullet = Instantiate(Bullet, bulletPosition, Quaternion.identity);
+        instBullet.GetComponent<BulletScript>().SetDirection(directionTarget);
     }
 
+    IEnumerator ShootAnimation()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Shoot(direction.normalized);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (collision.gameObject.tag == "Border")
+        //{
+        //    speed *= -1;
+        //    transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        //}
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         
