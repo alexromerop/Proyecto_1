19ce61d45c2b799d21d64944732aa2d3cfcfe9d8
alex <alex.romero@enti.cat>
2021-06-04 02:11:00 +0200

    MOVIMENTO HOJAS

---
+++ Assets/scr_feedback.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_feedback : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")|| collision.CompareTag("Enemy") || collision.CompareTag("Controlable"))
+        {
+            anim.SetTrigger("Move");
+        }
+    }
+}
